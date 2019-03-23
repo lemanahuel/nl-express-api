36f582fcbf4e98187850dfc53badd15daf0f7107
lemanahuel <lema.nahuel@gmail.com>
2019-03-23 20:29:22 -0300

    IMPROVE, docs, views

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const fs = require('fs');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
+const listEndpoints = require('express-list-endpoints');
 const config = require('./config/config');
 const db = require('./integrations/mongodb');
 const app = express();
 
 db.connect();
 
-app.set('views', path.join(__dirname, 'public/views'));
+app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/views')));
 app.use(cors());
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,6 +34,15 @@ fs.readdirSync(path.join(__dirname, 'modules/tasks/')).map(file => {
   if (file && file.includes('routes')) {
     require('./modules/tasks/' + file)(app);
   }
+});
+
+app.route('/docs').get((req, res) => {
+  res.render('docs/index', {
+    app: {
+      title: 'Render Express + Handlebars'
+    },
+    endpoints: listEndpoints(app)
+  });
 });
 
 app.listen(config.PORT, err => {
