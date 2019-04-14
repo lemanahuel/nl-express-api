3dd004f095ee2d1c48a36408a4cb5be670088500
lemanahuel <lema.nahuel@gmail.com>
2019-04-14 17:54:22 -0300

    IMPROVE, whitelist

--- config/config.js
+++ config/config.js
@@ -3,8 +3,11 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todo-list',
   CLOUDINARY_URL: process.env.CLOUDINARY_URL,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  WHITE_LIST_DOMAINS: ['http://localhost:5000', 'http://localhost:5001',
+  WHITE_LIST_DOMAINS: [
+    'http://localhost:5000',
+    'http://localhost:5001',
+    'http://nl-static-www.herokuapp.com',
     'https://nl-static-www.herokuapp.com',
-    'https://nl-express-www.herokuapp.com', 'https://nl-express-api.herokuapp.com',
-    'https://nl-express-www-prod.herokuapp.com', 'https://nl-express-api-prod.herokuapp.com']
-};
+    'https://nl-express-api.herokuapp.com',
+    'https://nl-express-api-prod.herokuapp.com']
+}; 
